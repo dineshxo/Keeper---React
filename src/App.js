f65fd8f07fc9de7ba9fb46d764e48fa58f1117db
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-01-26 23:20:09 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import Header from './Components/header';
 import Footer from './Components/footer';
 import Note from './Components/note';
-import notes from './data/notes';
+
+import AddNote from './Components/addNote';
 
 
 function App() {
+
+  const [notes, setNotes]=useState([]);
+
+
+  function newNote(newNote){
+      setNotes(prevNotes=>{
+      return [...prevNotes,newNote];
+   })
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes=>{
+    return prevNotes.filter((notesItem,index)=>{
+      return index!==id;
+    })
+ })
+}
+
+
+
+
   return (
     <div>
     <Header />
+    <AddNote onAdd={newNote}/>
+    
 
-{notes.map((notes)=>{
-  return <Note title={notes.title} content={notes.content} key={notes.id}/>
+{notes.map((notesItem,index)=>{
+  return <Note 
+    key={index}
+    id={index}
+    title={notesItem.title} 
+    content={notesItem.content} 
+    
+    onDelete={deleteNote}/>
 })}
 
     
